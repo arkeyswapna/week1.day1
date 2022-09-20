d2d683d997223b829a27eeafd7831bf32f9baf5f
arkeyswapna <114006179+arkeyswapna@users.noreply.github.com>
2022-09-20 20:42:26 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Car {
+private void driveCar() {
+	// TODO Auto-generated method stub
+System.out.println("Car is running sucessfully");
+}
+private void applyBreak() {
+	// TODO Auto-generated method stub
+	System.out.println("Break is applied successfully");
+}
+private void soundHorn() {
+	// TODO Auto-generated method stub
+	System.out.println("Horn is working sucessfully");
+}
+private void isPuncture() {
+	// TODO Auto-generated method stub
+	System.out.println("Tyre is punctured");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car c=new Car();
+c.driveCar();
+c.applyBreak();
+c.soundHorn();
+c.isPuncture();
+	}
+
+}
